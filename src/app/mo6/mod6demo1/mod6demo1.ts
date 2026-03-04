8163e603cb9b8e96df6e22a76c495a88291d2ecf
stropee <stropee@campus-eni.fr>
2026-03-04 13:42:49 +0100

    mod 6 form basique

--- src/app/mo6/mod6demo1/mod6demo1.ts
+++ src/app/mo6/mod6demo1/mod6demo1.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-mod6demo1',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './mod6demo1.html',
   styleUrl: './mod6demo1.css',
 })
 export class Mod6demo1 {
+
+  public form : FormGroup
+
+  constructor() {
+    this.form = new FormGroup(
+      {
+        email  : new FormControl('', [Validators.email, Validators.required]),
+        password  : new FormControl('', [Validators.pattern(/^[a-z0-9]{8,}$/i), Validators.required]),
+        date  : new FormControl('', [Validators.required]),//TODO control personnalisé date inférieur à date du jour
+        newsletter  : new FormControl(true),
+        job : new FormControl('chef', Validators.pattern(/^(dev|chef|admin)$/)),
+        note : new FormControl(5, [Validators.min(0), Validators.max(10)])
+      }
+    )
+  }
 
 }

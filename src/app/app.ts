57d1fa95847c9bc9b05c9ae3c29ef3a622f45399
stropee <stropee@campus-eni.fr>
2026-03-04 12:05:11 +0100

    mod 6 form basique

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,12 +3,13 @@ import {RouterOutlet} from '@angular/router';
 import {Demo1} from './mod3/demo1/demo1' ;
 import {Mod3demo2} from './mod3/mod3demo2/mod3demo2';
 import {Mod4demo1} from './mod4/mod4demo1/mod4demo1';
+import {Mod6demo1} from './mo6/mod6demo1/mod6demo1';
 
 // import {Demo1 as Truc} from './mod1/demo1/demo1';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Demo1, Mod3demo2, Mod4demo1,/*,Truc*/],
+  imports: [RouterOutlet, Demo1, Mod3demo2, Mod4demo1, Mod6demo1,/*,Truc*/],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/mo6/mod6demo1/mod6demo1.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mod6demo1',
+  imports: [],
+  templateUrl: './mod6demo1.html',
+  styleUrl: './mod6demo1.css',
+})
+export class Mod6demo1 {
+
+}
